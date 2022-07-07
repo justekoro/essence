1eb707c863e385dac9bf09223b0d5bfc1fa9ba8d
jokay03J <jokay03j@gmail.com>
2022-07-07 14:12:16 +0200

    update main page
    *transformation of almost all the elements by composants

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { useState } from "react";
 import { useEffect } from "react";
 import DataTable from "react-data-table-component";
 import Moment from "react-moment";
+import SelectTypeSearch from "./components/SelectTypeSearch";
+import InputSearch from "./components/InputSearch";
+import SelectTypeFuel from "./components/SelectTypeFuel";
+import SubmitButton from "./components/SubmitButton";
+import ResultComponents from "./components/ResultComponents";
 
 function App() {
   // state
@@ -14,30 +19,6 @@ function App() {
   const [searchType, setSearchType] = useState("City");
   const [typedFuel, setTypedFuel] = useState("All");
 
-  // variables
-  const columns = [
-    {
-      name: "Id",
-      selector: (row) => row["fields"].id,
-      sortable: true,
-    },
-    {
-      name: "prix",
-      selector: (row) => row["fields"].prix_valeur,
-      sortable: true,
-    },
-    {
-      name: "adresse",
-      selector: (row) => row["fields"].adresse,
-      sortable: true,
-    },
-    {
-      name: "type",
-      selector: (row) => row["fields"].prix_nom,
-      sortable: true,
-    },
-  ];
-
   // fetch data from API
   useEffect(() => {
     superagent
@@ -45,6 +26,8 @@ function App() {
         "https://data.economie.gouv.fr/api/records/1.0/search/?rows=-1&start=0&fields=id,adresse,com_arm_name,prix_maj,prix_nom,prix_valeur,prix_id,reg_code,reg_name,dep_code,dep_name,cp,com_arm_code,pop,geom&dataset=prix-carburants-fichier-instantane-test-ods-copie&timezone=Europe%2FBerlin"
       )
       .then((response) => {
+        if (!response.body.records || response.body.records.length === 0)
+          return alert("Aucune donnée trouvée");
         setData(response.body.records);
         console.log(response.body.records);
         setIsLoading(false);
@@ -61,14 +44,23 @@ function App() {
       case "City":
         typedFuel === "All"
           ? setTypedData(
-              data.filter((item) => item["fields"].com_arm_name === searchValue)
+              data.filter((item) => {
+                const str = `${item["fields"].com_arm_name}`;
+                const normalizedSearchValue = normalize(searchValue);
+                const normalizeStr = normalize(str);
+                return normalizeStr === normalizedSearchValue;
+              })
             )
           : setTypedData(
-              data.filter(
-                (item) =>
-                  item["fields"].com_arm_name === searchValue &&
+              data.filter((item) => {
+                const str = `${item["fields"].com_arm_name}`;
+                const normalizedSearchValue = normalize(searchValue);
+                const normalizeStr = normalize(str);
+                return (
+                  normalizeStr === normalizedSearchValue &&
                   item["fields"].prix_nom === typedFuel
-              )
+                );
+              })
             );
         break;
 
@@ -89,14 +81,23 @@ function App() {
       case "Dep":
         typedFuel === "All"
           ? setTypedData(
-              data.filter((item) => item["fields"].dep_name === searchValue)
+              data.filter((item) => {
+                const str = `${item["fields"].dep_name}`;
+                const normalizedSearchValue = normalize(searchValue);
+                const normalizeStr = normalize(str);
+                return normalizeStr === normalizedSearchValue;
+              })
             )
           : setTypedData(
-              data.filter(
-                (item) =>
-                  item["fields"].dep_name === searchValue &&
+              data.filter((item) => {
+                const str = `${item["fields"].dep_name}`;
+                const normalizedSearchValue = normalize(searchValue);
+                const normalizeStr = normalize(str);
+                return (
+                  normalizeStr === normalizedSearchValue &&
                   item["fields"].prix_nom === typedFuel
-              )
+                );
+              })
             );
         break;
 
@@ -117,17 +118,31 @@ function App() {
       default:
         typedFuel === "All"
           ? setTypedData(
-              data.filter((item) => item["fields"].com_arm_name === searchValue)
+              data.filter((item) => {
+                const str = `${item["fields"].com_arm_name}`;
+                const normalizedSearchValue = normalize(searchValue);
+                const normalizeStr = normalize(str);
+                return normalizeStr === normalizedSearchValue;
+              })
             )
           : setTypedData(
-              data.filter(
-                (item) =>
-                  item["fields"].com_arm_name === searchValue &&
+              data.filter((item) => {
+                const str = `${item["fields"].com_arm_name}`;
+                const normalizedSearchValue = normalize(searchValue);
+                const normalizeStr = normalize(str);
+                return (
+                  normalizeStr === normalizedSearchValue &&
                   item["fields"].prix_nom === typedFuel
-              )
+                );
+              })
             );
         break;
     }
+  };
+
+  //normalize function
+  const normalize = (str) => {
+    return str.toLowerCase().replace(/\s/g, "");
   };
 
   return (
@@ -155,70 +170,26 @@ function App() {
           <div className="Inputs-Container">
             <div className="Group-Container">
               {/* type of search */}
-              <select
-                className="Input-Type-Search"
+              <SelectTypeSearch
                 value={searchType}
                 onChange={(e) => setSearchType(e.target.value)}
-              >
-                <option disabled>Type de recherche</option>
-                <option value={"City"}>Vile</option>
-                <option value={"Postal-Code"}>Code Postal</option>
-                <option value={"Dep"}>Département</option>
-                <option value={"Dep-Code"}>Code Département</option>
-              </select>
+              />
               {/* search */}
-              <input
-                className="Input-Search"
-                value={searchValue}
+              <InputSearch
                 onChange={(e) => setSearchValue(e.target.value)}
+                value={searchValue}
               />
               {/* type fuel input */}
-              <select
-                className="Input-Type-Fuel"
-                value={typedFuel}
+              <SelectTypeFuel
                 onChange={(e) => setTypedFuel(e.target.value)}
-              >
-                <option value={"All"}>Tout</option>
-                <option value={"Gazole"}>Gazole</option>
-                <option value={"E85"}>E85</option>
-                <option value={"E10"}>E10</option>
-                <option value={"SP98"}>SP98</option>
-                <option value={"SP95"}>SP95</option>
-                <option value={"GPLc"}>GPLc</option>
-              </select>
+                value={typedFuel}
+              />
             </div>
             {/* submit button */}
-            <button className="Input-Submit" onClick={() => handleSubmit()}>
-              Envoyer
-            </button>
+            <SubmitButton onClick={() => handleSubmit()} />
           </div>
-          {typedData.length === 0 ? (
-            <div className="Not-found-Container">
-              <div className="Text">
-                Aucune donnée trouver
-                <br />
-                Veuillez bien vérifier la recherche entrer
-                <br />
-                n'oublier pas les majuscules et les accents
-                <br />
-                Ex: Saint-Priest ≠ saint-priest
-              </div>
-            </div>
-          ) : (
-            <div>
-              {/* results */}
-              <div className="Results">
-                <DataTable pagination columns={columns} data={typedData} />
-              </div>
-              {/* Update Date */}
-              <div className="Update-Container">
-                <div className="">mis à jour le: </div>
-                <Moment className="moment" format="YYYY/MM/DD">
-                  {Date.now()}
-                </Moment>
-              </div>
-            </div>
-          )}
+          {/* Results */}
+          <ResultComponents typedData={typedData} />
         </main>
       )}
     </div>
