b86ebda3635cc77fa566c7cd20a228011ccd2de3
jokay03J <jokay03j@gmail.com>
2022-07-24 20:09:54 +0200

    add geocode support and cron for download data in file

--- api/index.js
+++ api/index.js
@@ -1,13 +1,94 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config(".env");
-const cors = require('cors');
-//cors
-app.use(cors({ origin: '*' }));
-app.use(express.json());
-//routes
-app.use('/api', require('./routes/main-route'));
+require("dotenv").config(".env");
+const cors = require("cors");
+let geocoder = require("local-reverse-geocoder");
+var isGeocodeInitialized = false;
 
-app.listen(process.env.PORT || 3042, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-})
+// utils
+const getData = require("./utils/getData");
+
+//cors
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+
+// fetch and bump database
+let CronJob = require("cron").CronJob;
+let job = new CronJob(
+  "0 0 */5 * * *",
+  async function () {
+    await getData();
+  },
+  null,
+  true,
+  "Europe/Paris"
+);
+
+//routes
+app.use("/api", require("./routes/main-route"));
+
+app.get("/geocode", function (req, res) {
+  if (!isGeocodeInitialized) {
+    return res.status(503).send('Not ready yet.');
+  }
+
+  var lat = req.query.latitude || false;
+  var lon = req.query.longitude || false;
+  var maxResults = req.query.maxResults || 1;
+  if (!lat || !lon) {
+    return res.status(400).send('Bad Request');
+  }
+  var points = [];
+  if (Array.isArray(lat) && Array.isArray(lon)) {
+    if (lat.length !== lon.length) {
+      return res.status(400).send('Bad Request');
+    }
+    for (var i = 0, lenI = lat.length; i < lenI; i++) {
+      points[i] = { latitude: lat[i], longitude: lon[i] };
+    }
+  } else {
+    points[0] = { latitude: lat, longitude: lon };
+  }
+  geocoder.lookUp(points, maxResults, function (err, addresses) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.send(addresses);
+  });
+});
+
+// start cron dump server
+job.start();
+getData();
+console.log("server data dumped");
+
+app.listen(process.env.PORT || 3042, function () {
+  console.log('server is running on port ' + process.env.PORT || 3042);
+  console.log('Initializing Geocoder…');
+  console.log(
+    '(This may take a long time and will download ~300MB worth of data.)'
+  );
+  geocoder.init(
+    {
+      citiesFileOverride: 'cities15000',
+      load: {
+        admin1: true,
+        admin2: true,
+        admin3And4: true,
+        alternateNames: true,
+      },
+      countries: [],
+    },
+    function () {
+      console.log('Geocoder initialized and ready.');
+      console.log('Endpoints:');
+      console.log(` - http://localhost:${process.env.PORT || 3042}/api`);
+      console.log(`- http://localhost:${process.env.PORT || 3042}/geocode`);
+      console.log('Examples:');
+      console.log(
+        `- http://localhost:${process.env.PORT || 3042}/geocode?latitude=54.6875248&longitude=9.7617254`
+      );
+      isGeocodeInitialized = true;
+    }
+  );
+});
