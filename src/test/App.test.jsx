65f2c2d68eb1dda083c4a8ef693de1b09ddc5e6f
jokay03J <jokay03j@gmail.com>
2022-07-25 12:27:25 +0200

    remove client

--- src/test/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import renderer from "react-test-renderer";
-import App from "../App";
-
-it("renders correctly", () => {
-  const component = renderer.create(<App />);
-
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
