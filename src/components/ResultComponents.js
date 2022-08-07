43557ccd995aacbd1aa3536da9045f2e76eee994
jokay03J <magrojoan3@gmail.com>
2022-08-07 16:20:43 +0200

    update src

--- src/components/ResultComponents.js
+++ src/components/ResultComponents.js
@@ -1,60 +1,67 @@
 import React from "react";
-import DataTable from "react-data-table-component";
 import Moment from "react-moment";
 import ErrorOutlineIcon from "@mui/icons-material/ErrorOutline";
+import DirectionsIcon from "@mui/icons-material/Directions";
 import AlertDialog from "./DialogAdress";
+import Button from "@mui/material/Button";
+import { DataGrid } from "@mui/x-data-grid";
 import "../styles/App.scss";
+import { Box } from "@mui/material";
+import { ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material";
 
 const ResultComponents = ({ data }) => {
-  const customStyles = {
-    headRow: {
-      style: {
-        border: "none",
+  const customTheme = createTheme({
+    components: {
+      MuiTable: {
+        styleOverrides: { root: {
+          
+        } },
       },
     },
-    headCells: {
-      style: {
-        color: "#202124",
-        fontSize: "14px",
-      },
-    },
-    rows: {
-      style: {
-        overflow: "visible",
-      },
-      highlightOnHoverStyle: {
-        backgroundColor: "rgb(230, 244, 244)",
-        borderBottomColor: "#FFFFFF",
-        borderRadius: "25px",
-        outline: "1px solid #FFFFFF",
-      },
-    },
-    pagination: {
-      style: {
-        border: "none",
-      },
-    },
-  };
+  });
 
   const columns = [
     {
-      name: "Prix",
-      selector: (row) => {
-        return row.valeur;
+      field: "valeur",
+      headerName: "prix",
+      editable: false,
+      valueGetter: (params) => {
+        return `${params.row.valeur}`;
       },
-      sortable: true,
     },
     {
-      name: "Adresse",
-      selector: (row) => {
-        return <div className="Adress-Container">
-          <div className="mobile"><AlertDialog adress={row.adresse} city={row.ville}/></div>
-          <div className="desktop">{row.adresse}</div>
-        </div>
+      field: "adresse",
+      headerName: "Last name",
+      editable: false,
+      renderCell: (params) => {
+        return (
+          <div className="Adress-Container">
+            <div className="mobile">
+              <AlertDialog
+                adress={params.row.adresse}
+                city={params.row.ville}
+              />
+            </div>
+            <div className="desktop">
+              {params.row.adresse}
+              <Button
+                onClick={() =>
+                  window.open(
+                    `https://www.google.com/maps?q=${params.row.adresse} - ${params.row.ville}`
+                  )
+                }
+              >
+                <DirectionsIcon />
+              </Button>
+            </div>
+          </div>
+        );
       },
-      sortable: true,
     },
   ];
+
+  let id = 0;
 
   return (
     <div>
@@ -67,14 +74,33 @@ const ResultComponents = ({ data }) => {
         <div>
           {/* results */}
           <div className="Results">
-            <DataTable
+            {/* <DataTable
               pagination
               columns={columns}
               data={data}
               customStyles={customStyles}
               highlightOnHover
               pointerOnHover
-            />
+            /> */}
+            {/* <ThemeProvider theme={customTheme}> */}
+              <Box sx={{ height: 400, width: "100%", marginTop: 4 }}>
+                <DataGrid
+                  rows={data}
+                  columns={columns}
+                  pageSize={5}
+                  rowsPerPageOptions={[5]}
+                  disableSelectionOnClick
+                  disableColumnSelector
+                  editMode={false}
+                  autoHeight={true}
+                  sx={{ m: 2 }}
+                  getRowId={(row) => {
+                    id++;
+                    return id;
+                  }}
+                />
+              </Box>
+            {/* </ThemeProvider> */}
           </div>
           {/* Update Date */}
           <div className="Update-Container">
