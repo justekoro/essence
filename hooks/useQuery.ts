6b519a2d98ef2486072a0de2ab4a39003d862b4e
jokay03J <magrojoan3@gmail.com>
2024-01-28 13:26:56 +0100

    refactor(hooks): Update useQuery
    * Add comments
    * Add types to states
    * Update fetch url

--- hooks/useQuery.ts
+++ hooks/useQuery.ts
@@ -12,11 +12,12 @@ type Result = {
 
 export function useQuery(cp: string, fuel: string) {
     const [data, setData] = useState<Result[] | null>(null);
-    const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState<Error | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
     useEffect(() => {
-        const fuelName = fuel.slice(0, 1).toUpperCase() + fuel.slice(1, fuel.length)
-        const encodedUri = encodeURI(`https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=adresse,${fuel}_prix AS prix,id&where=cp = "${cp}" and carburants_disponibles in ("${fuelName}")&limit=-1`)
+        // to get only fuel price on request
+        const fuelName = fuel.toLowerCase();
+        const encodedUri = encodeURI(`https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=adresse,${fuelName}_prix AS prix,id&where=cp like "${cp}" and carburants_disponibles in ("${fuel}")&limit=-1`)
         setIsLoading(true);
         fetch(encodedUri)
             .then((r) => {
