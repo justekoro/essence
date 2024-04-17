4fda1c9c6f2eb19edfba12a387193028da7287d6
Jokay03J <magrojoan3@gmail.com>
2024-04-17 21:41:31 +0200

    fix(hooks): Fix wrong datasets url

--- hooks/useQuery.ts
+++ hooks/useQuery.ts
@@ -17,7 +17,7 @@ export function useQuery(cp: string, fuel: string) {
     useEffect(() => {
         // to get only fuel price on request
         const fuelName = fuel.toLowerCase();
-        const encodedUri = encodeURI(`https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-des-carburants-en-france-flux-instantane-v2/records?select=adresse,${fuelName}_prix AS prix,id&where=cp like "${cp}" and carburants_disponibles in ("${fuel}")&limit=-1`)
+        const encodedUri = encodeURI(`https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/prix-carburants-flux-instantane-v2/records?select=adresse,${fuelName}_prix AS prix,id&where=cp like "${cp}" and carburants_disponibles in ("${fuel}")&limit=-1`)
         setIsLoading(true);
         fetch(encodedUri)
             .then((r) => {
